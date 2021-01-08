11c67f5aba9682f29775fcc07795ae86504f64ad
Bruna Abatepaulo <bruna.abatepaulo@hotmail.com>
2021-01-08 13:27:28 -0300

    Responding to User Intarctions with IBActions - actions for the button

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     // connection to the image on the Main.storyboard
     // to delete the connection, right click on the image and delete the connection
     // to rename it, right click on the name and refactor it
+    // to connect just press control and drag the object to the code
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -22,6 +23,12 @@ class ViewController: UIViewController {
     // triggered when the app first shows up on the device
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+    }
+    
+    // triggered if the Roll button gets pressed
+    @IBAction func btnRollPressed(_ sender: UIButton) {
+//        print("Button got tapped") // print to the debug console
         
         // who.what = value
             // who needs to be changed
@@ -36,11 +43,8 @@ class ViewController: UIViewController {
 //        // changing the alpha (transparency) of the image
 //        diceImageView1.alpha = 0.5
         
-
-        
     }
     
-
 
 }
 
