8b36b5c1f69a4359c4ac1078ca3be0a34f65bd83
Bruna Abatepaulo <bruna.abatepaulo@hotmail.com>
2021-05-24 19:03:13 -0300

    added numbers after dice rolls

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,16 +14,14 @@ class ViewController: UIViewController {
         super.init(coder: coder)!
     }
     
-    // IBOutlet allows to reference to a UI element
-    // connection to the image on the Main.storyboard
-    // to delete the connection, right click on the image and delete the connection
-    // to rename it, right click on the name and refactor it
-    // to connect just press control and drag the object to the code
-    @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    @IBOutlet weak var diceImage1: UIImageView!
     
     @IBOutlet weak var segmentCheck: UISegmentedControl!
     
+    @IBOutlet weak var lblN1: UILabel!
+    
+    @IBOutlet weak var lblN2: UILabel!
     
     let redDiceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     let yellowDiceArray = [#imageLiteral(resourceName: "dice_yellow_01"), #imageLiteral(resourceName: "dice_yellow_02"), #imageLiteral(resourceName: "dice_yellow_03"), #imageLiteral(resourceName: "dice_yellow_04"), #imageLiteral(resourceName: "dice_yellow_05"), #imageLiteral(resourceName: "dice_yellow_06")]
@@ -36,15 +34,18 @@ class ViewController: UIViewController {
 
         switch index {
         case 0:
-            diceImageView1.image = redDiceArray[0]
-            diceImageView2.image = redDiceArray[0]
+            diceImage2.image = redDiceArray[0]
+            diceImage1.image = redDiceArray[0]
         case 1:
-            diceImageView1.image = yellowDiceArray[0]
-            diceImageView2.image = yellowDiceArray[0]
+            diceImage2.image = yellowDiceArray[0]
+            diceImage1.image = yellowDiceArray[0]
         default:
-            diceImageView1.image = redDiceArray[0]
-            diceImageView2.image = redDiceArray[0]
+            diceImage2.image = redDiceArray[0]
+            diceImage1.image = redDiceArray[0]
         }
+        
+        lblN1.text = ""
+        lblN2.text = ""
     }
     
     // triggered when the app first shows up on the device
@@ -52,9 +53,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 //        changeDice(array: [redDiceArray][0])
-        diceImageView1.image = redDiceArray[0]
-        diceImageView2.image = redDiceArray[0]
+        diceImage2.image = redDiceArray[0]
+        diceImage1.image = redDiceArray[0]
         
+        lblN1.text = ""
+        lblN2.text = ""
     }
     
     // triggered if the Roll button gets pressed
@@ -66,20 +69,58 @@ class ViewController: UIViewController {
         
         switch index {
         case 0:
-            diceImageView1.image = redDiceArray.randomElement()
-            diceImageView2.image = redDiceArray.randomElement()
+            diceImage2.image = redDiceArray.randomElement()
+            diceImage1.image = redDiceArray.randomElement()
         case 1:
-            diceImageView1.image = yellowDiceArray.randomElement()
-            diceImageView2.image = yellowDiceArray.randomElement()
+            diceImage2.image = yellowDiceArray.randomElement()
+            diceImage1.image = yellowDiceArray.randomElement()
         default:
-            diceImageView1.image = redDiceArray.randomElement()
-            diceImageView2.image = redDiceArray.randomElement()
+            diceImage2.image = redDiceArray.randomElement()
+            diceImage1.image = redDiceArray.randomElement()
+        }
+        
+        if diceImage1.image == #imageLiteral(resourceName: "DiceOne") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_01") {
+            lblN1.text = "1"
+        }
+        else if diceImage1.image == #imageLiteral(resourceName: "DiceTwo") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_02") {
+            lblN1.text = "2"
+        }
+        else if diceImage1.image == #imageLiteral(resourceName: "DiceThree") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_03") {
+            lblN1.text = "3"
+        }
+        else if diceImage1.image == #imageLiteral(resourceName: "DiceFour") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_04") {
+            lblN1.text = "4"
+        }
+        else if diceImage1.image == #imageLiteral(resourceName: "DiceFive") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_05") {
+            lblN1.text = "5"
+        }
+        else if diceImage1.image == #imageLiteral(resourceName: "DiceSix") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_06") {
+            lblN1.text = "6"
+        }
+        
+        if diceImage2.image == #imageLiteral(resourceName: "DiceOne") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_01") {
+            lblN2.text = "1"
+        }
+        else if diceImage2.image == #imageLiteral(resourceName: "DiceTwo") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_02") {
+            lblN2.text = "2"
+        }
+        else if diceImage2.image == #imageLiteral(resourceName: "DiceThree") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_03") {
+            lblN2.text = "3"
+        }
+        else if diceImage2.image == #imageLiteral(resourceName: "DiceFour") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_04") {
+            lblN2.text = "4"
+        }
+        else if diceImage2.image == #imageLiteral(resourceName: "DiceFive") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_05") {
+            lblN2.text = "5"
+        }
+        else if diceImage2.image == #imageLiteral(resourceName: "DiceSix") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_06") {
+            lblN2.text = "6"
         }
     }
     
     func changeDice(array: [UIImage]) {
-        diceImageView1.image = array.randomElement()
-        diceImageView2.image = array.randomElement()
+        diceImage2.image = array.randomElement()
+        diceImage1.image = array.randomElement()
     }
     
 
