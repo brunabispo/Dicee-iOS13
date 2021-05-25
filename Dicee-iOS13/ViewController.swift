844f51dfc5f274c13abe198db663eb8af88d054c
Bruna Abatepaulo <bruna.abatepaulo@hotmail.com>
2021-05-25 15:20:07 -0300

    added a different dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -25,6 +25,7 @@ class ViewController: UIViewController {
     
     let redDiceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     let yellowDiceArray = [#imageLiteral(resourceName: "dice_yellow_01"), #imageLiteral(resourceName: "dice_yellow_02"), #imageLiteral(resourceName: "dice_yellow_03"), #imageLiteral(resourceName: "dice_yellow_04"), #imageLiteral(resourceName: "dice_yellow_05"), #imageLiteral(resourceName: "dice_yellow_06")]
+    let pinkBlueDiceArray = [#imageLiteral(resourceName: "dice1"), #imageLiteral(resourceName: "dice2"), #imageLiteral(resourceName: "dice3"), #imageLiteral(resourceName: "dice4"), #imageLiteral(resourceName: "dice5"), #imageLiteral(resourceName: "dice6")]
     
     
     var diceArray: Array<UIImage>
@@ -39,6 +40,9 @@ class ViewController: UIViewController {
         case 1:
             diceImage2.image = yellowDiceArray[0]
             diceImage1.image = yellowDiceArray[0]
+        case 2:
+            diceImage2.image = pinkBlueDiceArray[0]
+            diceImage1.image = pinkBlueDiceArray[0]
         default:
             diceImage2.image = redDiceArray[0]
             diceImage1.image = redDiceArray[0]
@@ -74,46 +78,49 @@ class ViewController: UIViewController {
         case 1:
             diceImage2.image = yellowDiceArray.randomElement()
             diceImage1.image = yellowDiceArray.randomElement()
+        case 2:
+            diceImage2.image = pinkBlueDiceArray.randomElement()
+            diceImage1.image = pinkBlueDiceArray.randomElement()
         default:
             diceImage2.image = redDiceArray.randomElement()
             diceImage1.image = redDiceArray.randomElement()
         }
         
-        if diceImage1.image == #imageLiteral(resourceName: "DiceOne") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_01") {
+        if diceImage1.image == #imageLiteral(resourceName: "DiceOne") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_01") || diceImage1.image == #imageLiteral(resourceName: "dice1") {
             lblN1.text = "1"
         }
-        else if diceImage1.image == #imageLiteral(resourceName: "DiceTwo") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_02") {
+        else if diceImage1.image == #imageLiteral(resourceName: "DiceTwo") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_02") || diceImage1.image == #imageLiteral(resourceName: "dice2") {
             lblN1.text = "2"
         }
-        else if diceImage1.image == #imageLiteral(resourceName: "DiceThree") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_03") {
+        else if diceImage1.image == #imageLiteral(resourceName: "DiceThree") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_03") || diceImage1.image == #imageLiteral(resourceName: "dice3") {
             lblN1.text = "3"
         }
-        else if diceImage1.image == #imageLiteral(resourceName: "DiceFour") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_04") {
+        else if diceImage1.image == #imageLiteral(resourceName: "DiceFour") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_04") || diceImage1.image == #imageLiteral(resourceName: "dice4") {
             lblN1.text = "4"
         }
-        else if diceImage1.image == #imageLiteral(resourceName: "DiceFive") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_05") {
+        else if diceImage1.image == #imageLiteral(resourceName: "DiceFive") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_05") || diceImage1.image == #imageLiteral(resourceName: "dice5") {
             lblN1.text = "5"
         }
-        else if diceImage1.image == #imageLiteral(resourceName: "DiceSix") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_06") {
+        else if diceImage1.image == #imageLiteral(resourceName: "DiceSix") || diceImage1.image == #imageLiteral(resourceName: "dice_yellow_06") || diceImage1.image == #imageLiteral(resourceName: "dice6") {
             lblN1.text = "6"
         }
         
-        if diceImage2.image == #imageLiteral(resourceName: "DiceOne") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_01") {
+        if diceImage2.image == #imageLiteral(resourceName: "DiceOne") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_01") || diceImage2.image == #imageLiteral(resourceName: "dice1") {
             lblN2.text = "1"
         }
-        else if diceImage2.image == #imageLiteral(resourceName: "DiceTwo") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_02") {
+        else if diceImage2.image == #imageLiteral(resourceName: "DiceTwo") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_02") || diceImage2.image == #imageLiteral(resourceName: "dice2") {
             lblN2.text = "2"
         }
-        else if diceImage2.image == #imageLiteral(resourceName: "DiceThree") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_03") {
+        else if diceImage2.image == #imageLiteral(resourceName: "DiceThree") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_03") || diceImage2.image == #imageLiteral(resourceName: "dice3") {
             lblN2.text = "3"
         }
-        else if diceImage2.image == #imageLiteral(resourceName: "DiceFour") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_04") {
+        else if diceImage2.image == #imageLiteral(resourceName: "DiceFour") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_04") || diceImage2.image == #imageLiteral(resourceName: "dice4") {
             lblN2.text = "4"
         }
-        else if diceImage2.image == #imageLiteral(resourceName: "DiceFive") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_05") {
+        else if diceImage2.image == #imageLiteral(resourceName: "DiceFive") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_05") || diceImage2.image == #imageLiteral(resourceName: "dice5"){
             lblN2.text = "5"
         }
-        else if diceImage2.image == #imageLiteral(resourceName: "DiceSix") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_06") {
+        else if diceImage2.image == #imageLiteral(resourceName: "DiceSix") || diceImage2.image == #imageLiteral(resourceName: "dice_yellow_06") || diceImage2.image == #imageLiteral(resourceName: "dice6") {
             lblN2.text = "6"
         }
     }
@@ -122,12 +129,5 @@ class ViewController: UIViewController {
         diceImage2.image = array.randomElement()
         diceImage1.image = array.randomElement()
     }
-    
-
-    
-    
-    
-    
-
 }
 
